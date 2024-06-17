459056f329c6b7647a5ec6791252a7a7eb4a7f48
Parth <parthmmyadav@gmail.com>
2024-06-17 20:33:01 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require ('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get ('/twitter', (req, res) => {
+    res.send('<h1>Hello Twitter!</h1>')
+})
+
+app.listen(process.env.PORT, (req, res) => {
+    console.log(`Server is running on port ${port}`)
+}
+)
